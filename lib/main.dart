4ba6b776a8202c15a9e5b58aeff5f83c29feea20
Long Do <longdo@dobtech.vn>
2021-01-20 19:47:58 +0700

    connected to firebase and  firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,44 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/chat_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  Widget _buildHomeScreen(AsyncSnapshot snapshot) {
+    if (snapshot.hasError) {
+      return Center(
+        child: Text('An error occured...'),
+      );
+    }
+    if (snapshot.connectionState == ConnectionState.done) {
+      return ChatScreen();
+    }
+    return Center(
+      child: Text('Loading...'),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FlutterChat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: ChatScreen(),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        return MaterialApp(
+          title: 'FlutterChat',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          home: _buildHomeScreen(snapshot),
+        );
+      },
     );
   }
 }
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,8 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CollectionReference messages = FirebaseFirestore.instance
+        .collection('chats/L6G6DeTGPzMla48dc3w0/messages');
+
     return Scaffold(
       body: ListView.builder(
         itemCount: 10,
@@ -10,6 +14,16 @@ class ChatScreen extends StatelessWidget {
           padding: EdgeInsets.all(8),
           child: Text('This works!'),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          messages.snapshots().listen((snapshop) {
+            snapshop.docs.forEach((doc) {
+              print(doc['text']);
+            });
+          });
+        },
+        child: Icon(Icons.add),
       ),
     );
   }

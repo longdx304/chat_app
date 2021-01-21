1f7be19aba0f92255f816b91b6a0a086d1ff8f6d
Long Do <longdo@dobtech.vn>
2021-01-21 11:50:40 +0700

    add form validator

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -6,6 +6,20 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  var _userEmail = '';
+  var _userPassword = '';
+  var _userName = '';
+
+  void _submitForm() {
+    final isValid = _formKey.currentState.validate();
+    if (!isValid) return;
+    _formKey.currentState.save();
+    print(_userEmail);
+    print(_userPassword);
+    print(_userName);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,21 +29,41 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    onSaved: (newValue) => _userEmail = newValue,
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@'))
+                        return 'Please enter a valid email address';
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
                       labelText: 'Email address',
                     ),
                   ),
                   TextFormField(
+                    onSaved: (newValue) => _userName = newValue,
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 4)
+                        return 'Please enter at least 4 characters';
+                      return null;
+                    },
                     decoration: InputDecoration(
                       labelText: 'Username',
                     ),
                   ),
                   TextFormField(
+                    onSaved: (newValue) => _userPassword = newValue,
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7)
+                        return 'Password must be at least 7 character long';
+                      return null;
+                    },
                     obscureText: true,
                     decoration: InputDecoration(
                       labelText: 'Password',
@@ -37,7 +71,7 @@ class _AuthFormState extends State<AuthForm> {
                   ),
                   SizedBox(height: 12),
                   ElevatedButton(
-                    onPressed: () {},
+                    onPressed: _submitForm,
                     child: Text('Login'),
                   ),
                   TextButton(
